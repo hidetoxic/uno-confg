5c50cac18e6007a9fbf13c7673264035703f7d72
Hidetodong <121945480@qq.com>
2022-11-13 17:24:10 +0800

    feat: 新增uno配置

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-export const one = 1
-export const two = 2
+import { presetAttributify, presetUno } from 'unocss'
+
+export function presets(): any[] {
+  return [presetAttributify(), presetUno()]
+}
+
+export function rules(): any[] {
+  return []
+}
+
+export function shortcuts(): any[] {
+  return [
+    [/^btn-(.*)$/, ([, c]: any) => `bg-${c}-400 text-${c}-100 py-2 px-4 rounded-lg`],
+  ]
+}
+
+export function config(): Record<string, any> {
+  return {
+    presets: presets(),
+    rules: rules(),
+    shortcuts: shortcuts(),
+  }
+}
+
